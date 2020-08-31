25c662d4279a432247594c2de02cecf2107bb394
chinna40 <chinna40@yahoo.com>
2020-08-31 11:03:07 +0100

    Update PortfolioService.java

--- src/com/swissre/PortfolioService.java
+++ src/com/swissre/PortfolioService.java
@@ -6,14 +6,19 @@ import java.util.Map;
 
 public class PortfolioService {
     public void convertPortfolio(Map<String, String> cryptoCoins){
-        Map<String, String> portfolioEurValue = new HashMap<>();
+        Map<String, BigDecimal> portfolioEurValue = new HashMap<>();
         ForexService forexService = new ForexService();
         for (Map.Entry<String, String> entry : cryptoCoins.entrySet()) {
             final BigDecimal eurValue = forexService.convert(entry.getKey(), "EUR");
             final BigDecimal cryptoAmount = new BigDecimal(entry.getValue());
             final BigDecimal cryptoEurValue = cryptoAmount.multiply(eurValue);
-
+            portfolioEurValue.put(entry.getKey(), cryptoEurValue);
         }
+        portfolioEurValue.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + "  : " + entry.getValue());
+        });
 
+        BigDecimal total = portfolioEurValue.values().stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println( " Total : " + total.toPlainString());
     }
 }
